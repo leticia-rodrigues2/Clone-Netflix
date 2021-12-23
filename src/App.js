b4924b20b21cd92fd345053f7d138abaff4285f9
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2021-12-23 00:22:53 -0300

    cards criados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import './App.css'
+
 export default () =>{
+  const [movieList , setMovieList] = useState([]);
+
+
+  useEffect(() => {
+      const loadAll = async () => {
+        //Lista total
+        let list = await Tmdb.getHomeList();
+        console.log(list);
+        setMovieList(list);
+      
+      }
+  loadAll();
+  },[]);
+
   return(
-<div>
-  Olá mundo !
-</div>
-  );
+  <div className="page">
+    <section className="lists">
+      {movieList.map((item, key)=>
+      <div>
+        <MovieRow key={key} title ={item.title} items ={item.items}/>
+      </div>
+      )}
+    </section>
+    </div>
+   );
 }
